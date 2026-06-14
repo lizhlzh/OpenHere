c159289582edccacc0d51319694bdbc797c9e0dc
lizhlzh95 <lizhlzh@outlook.com>
2026-06-14 12:57:49 +0800

    Initial Commit

---
+++ OpenHere/OpenHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenHereApp.swift
+//  OpenHere
+//
+//  Created by lzh on 2026/6/14.
+//
+
+import SwiftUI
+
+@main
+struct OpenHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
